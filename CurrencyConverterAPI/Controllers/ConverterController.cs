11182c6059008543d2c1cff2c7d5c17c0bdda674
Bui Minh Quan <buiminhquan.hcmus@gmail.com>
2021-11-06 18:59:20 +0700

    Add converter controller and model.

---
+++ CurrencyConverterAPI/Controllers/ConverterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConverterController : Controller
+    {
+        // GET: api/Converter
+        [HttpGet("convert/{amount}/{exchangeRate}")]
+        public float convert(float amount, float exchangeRate)
+        {
+            CurrencyConverterAPI.Models.Converter converter = new Models.Converter(amount, exchangeRate);
+            converter.convert();
+            return converter.result;
+        }
+    }
+}
---
+++ CurrencyConverterAPI/Models/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterAPI.Models
+{
+    public class Converter
+    {
+        public float amount;
+        public float exchangeRate;
+        public float result;
+
+        public Converter(float amount, float exchangeRate)
+        {
+            this.amount = amount;
+            this.exchangeRate = exchangeRate;
+        }
+
+        public void convert()
+        {
+            result = amount * exchangeRate;
+        }
+    }
+}
